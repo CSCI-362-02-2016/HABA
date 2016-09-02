a1cb7f8c27f772dc9f51635fb8129f27804606f5
Felkelhl <felkelhl@g.cofc.edu>
2016-09-01 23:02:51 -0400

    Updated Spacing

--- myList.sh
+++ myList.sh
@@ -1,1 +1,6 @@
-ls > lsOutput.html | xdg-open lsOutput.html
+printf "<html>" tee output2.html
+for file in *; do
+    printf "%s <br>" $file 
+done | tee output2.html
+printf "</html>" tee output2.html
+xdg-open output2.html
