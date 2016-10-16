d5a0d8b46f58d164a7cac020d5bc128f86904848
Felkelhl <felkelhl@g.cofc.edu>
2016-10-15 22:29:54 -0400

    Update testDriver.java

--- TestAutomation/testCasesExecutables/testCasePackage/testDriver.java
+++ TestAutomation/testCasesExecutables/testCasePackage/testDriver.java
@@ -19,10 +19,12 @@ public class testDriver {
 
 		Method methodList[] = cls2.getDeclaredMethods();
 		//Method methodList2[] = f.getClass().getDeclaredMethods();
-		System.out.println(methodList[0].getDeclaringClass());
+		System.out.println("Class: + " + methodList[0].getDeclaringClass());
 		for(int i = 0; i < methodList.length; i++){
-			System.out.println(methodList[i].getName());
+			System.out.println("Method " + methodList[i].getName());
 		}
+		// Test case read from file will go below
+		// Sample run
 		char[] password = "test".toCharArray();
 		if(MartusUserNameAndPassword.isWeakPassword(password) == false){
 			System.out.println("False");
@@ -33,10 +35,5 @@ public class testDriver {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-	}
-}
-class Methodtest2 {
-	public static int add(int a, int b){
-		return a+b;
 	}
 }
