790bbf23ebf756bc585613e3c8e666b64a56021e
hugo <Felkelhl@g.cofc.edu>
2016-10-20 20:15:38 -0400

    Updated shell

--- TestAutomation/scripts/runAllTest.sh
+++
@@ -1,9 +0,0 @@
-# 
-#
-
-# Compile the driver
-javac -cp ../project ../testCasesExecutables/testCasePackage/testDriver.java
-
-# Run the driver
-java -cp ../project/Libraries/martus.jar:../testCasesExecutables testCasePackage.testDriver
-
