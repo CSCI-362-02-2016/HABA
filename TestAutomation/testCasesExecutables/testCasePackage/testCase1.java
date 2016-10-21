1444b6ca5cc85d675c6219e02c5e42f14e1f8ff1
hugo <Felkelhl@g.cofc.edu>
2016-10-20 20:30:46 -0400

    Updated names

--- TestAutomation/testCasesExecutables/testCasePackage/testCase1.java
+++ TestAutomation/testCasesExecutables/testCasePackage/testCase1.java
@@ -2,13 +2,9 @@ package testCasePackage;
 
 import org.martus.client.core.MartusUserNameAndPassword;
 
-public class testCaseA {
+public class testCase1 {
 	// pass input which comes from the script file
 	// Perhaps pass in the Oracle
-	public boolean drive(String input){ 
-		return MartusUserNameAndPassword.isWeakPassword(input.toCharArray());
-		
-	}
 	public static void main(String args[]){
 		System.out.println(MartusUserNameAndPassword.isWeakPassword(args[0].toCharArray()));
 	}
