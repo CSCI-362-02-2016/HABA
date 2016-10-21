5357498fede4579794a272a944e2b6b36baeff83
hugo <Felkelhl@g.cofc.edu>
2016-10-20 20:09:45 -0400

    Changes

--- HABA-master/TestAutomation/scripts/runAllTests.sh
+++ HABA-master/TestAutomation/scripts/runAllTests.sh
@@ -1,22 +1,8 @@
- # Display all the files within the testCase folder
+# 
+#
 
-for filename in testCases/*.txt; do
-  echo "$filename"
-done
+# Compile the driver
+javac -cp ./project ./testCasesExecutables/testCasePackage/testDriver.java
 
-
-# Pulling the testCase driver from the test case
-for filename in testCases/*; do
-	
-	driver=$(sed -n '1p' "$filename")
-	input=$(sed -n '6p' "$filename")
-	echo "Testing $driver with the input of: $input"
-	# Compile the driver
-javac -cp ./project ./testCasesExecutables/testCasePackage/$driver.java
-
-# Run the driver - Needs to get variable name from textfile
-java -cp ./project/Libraries/martus.jar:./testCasesExecutables testCasePackage.$driver $input
-	echo $variable
-done
-
-
+# Run the driver
+java -cp ./project/Libraries/martus.jar:./testCasesExecutables testCasePackage.testDriver
